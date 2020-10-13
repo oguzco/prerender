ac1ff7e6b918f9eefeec1ff8163cf21edea8bbb5
Oğuz Çiloğlu <45456335+oguzco@users.noreply.github.com>
2020-10-13 11:13:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+const prerender = require('prerender'); //const server = prerender();
 
-server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+var server = prerender({
+        chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu','--remote-debugging-port=9222', '--hide-scrollbars'],
+        pageDoneCheckInterval: 50,
+        blacklistedDomains: ['embed.tawk.to','cdn.jsdelivr.net','static-v.tawk.to','va.tawk.to','portalv3.s3.eu-central-1.amazonaws.com','www.googletagmanager.com','fonts.googleapis.com'],
+        pageLoadTimeout: 30 * 1000,
+        waitAfterLastRequest: 1000,
+        followRedirects: true
+});
+
+const cacheModule =  require('prerender-memory-cache') ;
+exports.CACHE_MAXSIZE=1000;
+exports.CACHE_TTL=600;
+
+server.use(cacheModule);
 
 server.start();
