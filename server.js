76080c11335db21c2ae55c35b797f545f95d4ec6
Oguz Ciloglu <oguz@ciloglu.us>
2020-10-13 12:01:28 +0300

    reset

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-var prerender = require('./lib');
+const prerender = require('prerender'); //const server = prerender();
+const child_process = require('child_process');
 
-const prerender2 = require('prerender'); //const server = prerender();
-
-var server = prerender2({
+var server = prerender({
         chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu','--remote-debugging-port=9222', '--hide-scrollbars'],
         pageDoneCheckInterval: 50,
         blacklistedDomains: ['embed.tawk.to','cdn.jsdelivr.net','static-v.tawk.to','va.tawk.to','portalv3.s3.eu-central-1.amazonaws.com','www.googletagmanager.com','fonts.googleapis.com'],
@@ -17,5 +15,10 @@ exports.CACHE_MAXSIZE=1000;
 exports.CACHE_TTL=600;
 
 server.use(cacheModule);
+
+
+child_process.execFile("chrome.sh", [], (error, stdout, stderr) => {
+	console.log("Startup Script",stdout);
+});
 
 server.start();
